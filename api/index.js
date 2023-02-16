2014b1235ea8bb3c3aa664eac9b15784c4475a84
vikash93825 <vikashkumar.kk@gmail.com>
2023-02-16 14:39:57 +0530

    LOgin Page and register page completed

---
+++ api/index.js
@@ -0,0 +1,80 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
+const User = require("./models/User.js");
+require("dotenv").config();
+
+const app = express();
+
+const bcryptSalt = bcrypt.genSaltSync(10);
+const jwtSecret = "cdjsdkjncdhgcjskdsjhdb";
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+  cors({
+    credentials: true,
+    origin: "http://127.0.0.1:5173",
+  })
+);
+
+mongoose.connect(process.env.MONGO_URL);
+
+app.get("/test", (req, res) => {
+  res.json("test ok");
+});
+
+app.post("/register", async (req, res) => {
+  const { name, email, password } = req.body;
+  try {
+    const userDoc = await User.create({
+      name,
+      email,
+      password: bcrypt.hashSync(password, bcryptSalt),
+    });
+    res.json(userDoc);
+  } catch (e) {
+    res.status(422).json(e);
+  }
+});
+
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  const userDoc = await User.findOne({ email });
+  if (userDoc) {
+    const passOK = bcrypt.compareSync(password, userDoc.password);
+    if (passOK) {
+      jwt.sign(
+        { email: userDoc.email, id: userDoc._id },
+        jwtSecret,
+        {},
+        (err, token) => {
+          if (err) throw err;
+          res.cookie("token", token).json(userDoc);
+        }
+      );
+    } else {
+      res.status(422).json("pass not ok");
+    }
+  } else {
+    res.json("failed");
+  }
+});
+
+app.get("/profile", (req, res) => {
+  const { token } = req.cookies;
+  if (token) {
+    jwt.verify(token, jwtSecret, {}, async (err, userData) => {
+      if (err) throw err;
+      const { name, email, _id } = await User.findById(userData.id);
+      res.json({ name, email, _id });
+    });
+  } else {
+    res.json(null);
+  }
+});
+
+app.listen(4000);
---
+++ client/src/pages/RegisterPage.jsx
@@ -0,0 +1,52 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
+
+export default function RegisterPage() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const registerUser = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.post("/register", { name, email, password });
+    } catch (error) {
+      alert("Registeration Failed");
+    }
+  };
+  return (
+    <div className="mt-4 grow flex items-center justify-around">
+      <div className="mb-64">
+        <h1 className="text-4xl text-center mb-4">Register</h1>
+        <form className="max-w-md mx-auto" onSubmit={registerUser}>
+          <input
+            type="text"
+            placeholder="username"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <input
+            type="email"
+            placeholder="your@email.com"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button className="primary">Register</button>
+          <div className="text-center py-2 text-gray-500">
+            Already a member?{" "}
+            <Link className="text-black underline" to={"/login"}>
+              Login
+            </Link>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
