b9465f8b87e289642277a1dc5e554bba0ed6dea3
vikash93825 <vikashkumar.kk@gmail.com>
2023-04-16 11:38:54 +0530

    added booking details

--- client/src/pages/IndexPage.jsx
+++ client/src/pages/IndexPage.jsx
@@ -13,7 +13,7 @@ export default function IndexPage() {
     <div className="mt-8 grid gap-x-6 gap-y-8 grid-cols-2 md:gris-cols-3 lg:grid-cols-4">
       {places.length > 0 &&
         places.map((place) => (
-          <Link to={"/place/" + place._id}>
+          <Link to={"/place/" + place._id} key={place._id}>
             <div className="bg-gray-500 mb-2 rounded-2xl flex">
               {place.photos?.[0] && (
                 <img
