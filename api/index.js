c4da0143172a20f475f41dad22367cae03ae038a
vikash93825 <vikashkumar.kk@gmail.com>
2023-06-17 22:39:06 +0530

    added booking details page

--- api/index.js
+++ api/index.js
@@ -6,9 +6,12 @@ const jwt = require("jsonwebtoken");
 const cookieParser = require("cookie-parser");
 const User = require("./models/User.js");
 const Place = require("./models/Place.js");
+const Booking = require("./models/Booking.js");
 const imageDownloader = require("image-downloader");
 const multer = require("multer");
 const fs = require("fs");
+const { resolve } = require("path");
+const { rejects } = require("assert");
 
 require("dotenv").config();
 const app = express();
@@ -27,6 +30,15 @@ app.use(
 );
 
 mongoose.connect(process.env.MONGO_URL);
+
+function getUserDataFromToken(req) {
+  return new Promise((resolve, reject) => {
+    jwt.verify(req.cookies.token, jwtSecret, {}, async (err, userData) => {
+      if (err) throw err;
+      resolve(userData);
+    });
+  });
+}
 
 app.get("/test", (req, res) => {
   res.json("test ok");
@@ -198,4 +210,32 @@ app.put("/places", async (req, res) => {
 app.get("/places", async (req, res) => {
   res.json(await Place.find());
 });
+
+app.post("/bookings", async (req, res) => {
+  const userData = await getUserDataFromToken(req);
+  const { place, checkIn, checkOut, numberOfGuests, name, mobile, price } =
+    req.body;
+  Booking.create({
+    place,
+    checkIn,
+    checkOut,
+    numberOfGuests,
+    name,
+    mobile,
+    price,
+    user: userData.id,
+  })
+    .then((doc) => {
+      res.json(doc);
+    })
+    .catch((err) => {
+      throw err;
+    });
+});
+
+app.get("/bookings", async (req, res) => {
+  const userData = await getUserDataFromToken(req);
+  res.json(await Booking.find({ user: userData.id }).populate("place"));
+});
+
 app.listen(4000);
